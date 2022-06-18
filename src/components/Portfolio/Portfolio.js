46058c1e304f651708fd0ef0173de7eef796b241
Evgeny Synkov <synkoveugeny@gmail.com>
2022-06-18 21:06:05 +0300

    Исправлены ссылки в портфолио

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,15 +7,15 @@ function Portfolio() {
     <div className="portfolio">
       <h3 className="portfolio__title">Портфолио</h3>
       <div className="portfolio__links">
-        <a className="portfolio__link" href="https://github.com/Synkov2102/how-to-learn">
+        <a target="_blank" className="portfolio__link" href="https://github.com/Synkov2102/how-to-learn">
           <p className="portfolio__link-title">Статичный сайт</p>
           <img className="portfolio__link-icon" src={arrowPath} alt="Изображение стрелочки для ссылки"/>
         </a>
-        <a className="portfolio__link" href="https://github.com/Synkov2102/russian-travel">
+        <a target="_blank" className="portfolio__link" href="https://github.com/Synkov2102/russian-travel">
           <p className="portfolio__link-title">Адаптивный сайт</p>
           <img className="portfolio__link-icon" src={arrowPath} alt="Изображение стрелочки для ссылки"/>
         </a>
-        <a className="portfolio__link" href="https://github.com/Synkov2102/react-mesto-api-full">
+        <a  target="_blank" className="portfolio__link" href="https://github.com/Synkov2102/react-mesto-api-full">
           <p className="portfolio__link-title">Одностраничное приложение</p>
           <img className="portfolio__link-icon" src={arrowPath} alt="Изображение стрелочки для ссылки"/>
         </a>
